f72f73f0253ccbbfa691112c041e1abfd45733f4
Gerard Arnaiz <astor98gas@proton.me>
2026-02-06 13:07:22 +0100

    Init Commit

---
+++ src/main/java/com/arsansys/siva/ProjectSivaApplication.java
@@ -0,0 +1,13 @@
+package com.arsansys.siva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arsansys/siva/ProjectSivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsansys.siva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
